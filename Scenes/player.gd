ce220c4db8c97e65150871bcf7d39cf20e30f210
pooya <pooyana84@gmail.com>
2024-06-14 14:44:24 +0330

    add instructions to queue / update destionation from current instruction / clean up

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,59 +1,60 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const BLOCKSIZE = 32
 
-const diff = 32
-var direction_sig = []
-var on_process = false
-var instruction_dir = [ 0, 0 ]
-var destination = []
 @export var direction: int = 0
 
+var instruction_queue = []
+var on_process = false
+var instruction = 0
+var destination = []
+
 func move_right():
-	direction_sig.push_back([ 1, 0 ])
+	instruction_queue.push_back(4)
 	
 func move_left():
-	direction_sig.push_back([ -1, 0 ])
+	instruction_queue.push_back(5)
 	
 func move_up():
-	direction_sig.push_back([ 0, -1 ])
+	instruction_queue.push_back(6)
 
 func move_down():
-	direction_sig.push_back([ 0, 1 ])
+	instruction_queue.push_back(7)
 	
 func turn_clockwise():
-	direction_sig.push_back([ 2, 0 ])
+	instruction_queue.push_back(1)
 	
 func turn_counter():
-	direction_sig.push_back([ 0, 2 ])
+	instruction_queue.push_back(2)
 	
 func move_forward():
-	direction_sig.push_back([ 1, 1 ])
+	instruction_queue.push_back(3)
 
 func _physics_process(delta):
-	if not on_process and direction_sig.size() > 0:
-		instruction_dir = direction_sig.pop_front()
-		
-		var dirX = instruction_dir[0]
-		var dirY = instruction_dir[1]
-		
-		if dirX == 1 and dirY == 0:
-			destination = [ position.x + diff, 0 ]
-		elif dirX == -1 and dirY == 0:
-			destination = [ position.x - diff, 0 ]
-		elif dirX == 0 and dirY == 1:
-			destination = [ position.x, position.y + diff ]
-		elif dirX == 0 and dirY == -1:
-			destination = [ position.x, position.y - diff ]
-		
+	if not on_process and instruction_queue.size() > 0:
+		instruction = instruction_queue.pop_front()
+		destination = [self.position.x, self.position.y]
+		match instruction:
+			1:
+				direction = (direction + 1) % 4
+			2:
+				direction = (direction + 3) % 4
+			3:
+				match direction:
+					0: destination[1] -= BLOCKSIZE
+					1: destination[0] += BLOCKSIZE
+					2: destination[1] += BLOCKSIZE
+					3: destination[0] -= BLOCKSIZE
+			4: destination[0] += BLOCKSIZE
+			5: destination[0] -= BLOCKSIZE
+			6: destination[1] -= BLOCKSIZE
+			7: destination[1] += BLOCKSIZE
 		on_process = true
 	
 	if on_process:
-		pass
+		pass #TODO
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var directionX = Input.get_axis("ui_left", "ui_right")
 	var directionY = Input.get_axis("ui_up", "ui_down")
 	if directionX:
