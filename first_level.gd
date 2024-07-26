9a79e7b3b907e194f3f10f24dbc325a7791e393f
pooya <pooyana84@gmail.com>
2024-07-26 12:15:06 +0330

    level loads from code

---
+++ first_level.gd
@@ -0,0 +1,26 @@
+extends TileMap
+
+var grnd = Vector2i(0, 0)
+var wall = Vector2i(1, 0)
+var lava = Vector2i(2, 0)
+var watr = Vector2i(3, 0)
+
+var test_level = [
+	[wall, wall, wall, wall, wall, wall, wall, wall, wall, wall],
+	[wall, grnd, grnd, grnd, wall, wall, grnd, grnd, watr, wall],
+	[wall, grnd, wall, grnd, wall, wall, grnd, wall, wall, wall],
+	[wall, grnd, wall, grnd, wall, wall, grnd, grnd, grnd, wall],
+	[wall, grnd, wall, grnd, wall, wall, wall, wall, grnd, wall],
+	[wall, grnd, wall, grnd, grnd, grnd, grnd, grnd, grnd, wall],
+	[wall, wall, wall, wall, wall, wall, wall, wall, wall, wall]	
+]
+
+func _ready():
+	for i in range(0, 10):
+		for j in range(0, 6):
+			self.set_cell(0, Vector2i(i,j),  0, test_level[j][i])
+
+
+
+func _process(delta):
+	pass
