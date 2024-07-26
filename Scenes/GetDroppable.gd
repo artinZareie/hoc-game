42c164d5fa85dd274e2e2ea4d19c0c318dfcff2b
Artin Zarei <artin.zareie1383@gmail.com>
2024-07-26 13:05:04 +0330

    Add classes

--- Scenes/GetDroppable.gd
+++ Scenes/GetDroppable.gd
@@ -1,5 +1,7 @@
 extends VBoxContainer
 
+var statements: Array = []
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
---
+++ Scripts/CodeClass.gd
@@ -0,0 +1,4 @@
+class_name CodeClass
+
+var name
+
